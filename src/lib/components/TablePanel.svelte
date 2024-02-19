cfb9eda0dd3f3b836fb4c1fc98ec8d2c1848ca81
chejel <chejenj@gmail.com>
2024-02-19 01:05:29 -0500

    Add table panel

--- src/lib/components/TablePanel.svelte
+++ src/lib/components/TablePanel.svelte
@@ -1,7 +1,76 @@
 <script>
-	import { selectedIntlCity } from '$lib/stores.js';
+	import { map, selectedIntlCity, citiesData } from '$lib/stores.js';
+
+	// Import components
+	import ResetMap from '$lib/components/ResetMap.svelte';
+	import ArrowLeft from '$lib/components/icons/ArrowLeft.svelte';
+
+	$: filteredData = $citiesData
+		.filter((d) => d.intlCity.properties.name === $selectedIntlCity)
+		.map((d) => d.usCity.properties)
+		// Add index
+		.map((d, i) => ({ ...d, index: i + 1 }));
+
+	$: selectedCountry = [
+		...new Set(
+			$citiesData
+				.filter((d) => d.intlCity.properties.name === $selectedIntlCity)
+				.map((d) => ({
+					country: d.intlCity.properties.country,
+					flagEmoji: d.intlCity.properties.flag_emoji
+				}))
+		)
+	][0];
 </script>
 
-<!-- Back arrow -->
+{#if $selectedIntlCity}
+	<!-- Back arrow -->
+	<ResetMap parentComponent="TablePanel">
+		<ArrowLeft />
+	</ResetMap>
 
-<!-- Table of matching city names, state, Wikipedia link-->
+	<!-- Headline -->
+	{$selectedIntlCity}, {selectedCountry.country}
+	{selectedCountry.flagEmoji}
+	{$citiesData.length > 1 ? 'matches' : 'match'}
+	{filteredData.length} U.S. {filteredData.length > 1 ? 'municipalities' : 'municipality'}
+
+	<!-- Table of matching city names, state, Wikipedia link-->
+	<div class="table-container">
+		<table>
+			<colgroup>
+				<col style="width: 50px" />
+				<col style="width: auto" />
+				<col style="width: 150px" />
+			</colgroup>
+			<thead>
+				<tr>
+					<th scope="col">No.</th>
+					<th scope="col">City</th>
+					<th scope="col">State</th>
+				</tr></thead
+			>
+			<tbody>
+				{#each filteredData as { index, name, state }}
+					<tr>
+						<td class="index">{index}</td>
+						<td class="value" on:click={() => console.log(name)}>{name} </td>
+						<td>{state}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+{/if}
+
+<style>
+	.table-container {
+		overflow: auto;
+	}
+	table {
+		width: 100%;
+	}
+	th {
+		text-align: left;
+	}
+</style>
