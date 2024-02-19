d9a7fde03fef4682564252236ae99bc3ecf3055b
chejel <chejenj@gmail.com>
2024-02-19 01:02:48 -0500

    Add store for selected city

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
 
-export let map = writable(null);
-export let citiesData = writable([])
-export let citiesDataFC = writable([])
+export let map = writable();
+export let citiesData = writable([]) // dataset
+export let citiesDataFC = writable([]) // dataset as feature collection
+export let selectedIntlCity = writable() // selected int'l city
+export let matchingUSCities = writable([]) // array of US cities matching selected int'l city
