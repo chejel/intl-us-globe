1ba2116594cd3437b934535a607879ffc579367d
chejel <chejenj@gmail.com>
2024-02-23 23:28:50 -0500

    Edit text

--- src/lib/components/AboutPanel.svelte
+++ src/lib/components/AboutPanel.svelte
@@ -8,29 +8,32 @@
 <main class="panel-body">
 	<section class="introduction">
 		<p>
-			The points on the globe represent major cities whose English-language name matches a
-			municipality in the U.S., either fully or in part. The names may or may not have a shared
+			The points on the globe represent major cities whose English-language name matches one or more
+			municipalities in the U.S., either fully or in part. The names may or may not have a shared
 			history; the only relationship meant to be conveyed here is the similarity in name only. The
 			international cities only include those with a population greater than 1 million in the city
 			proper and not the metropolitan area. The U.S. municipality, on the other hand, includes any
-			populated entity from cities and towns to villages and unincorporated communities.
+			populated entity, from cities and towns to villages and unincorporated communities.
 		</p>
 	</section>
 
 	<section class="subsection">
 		<span class="subheader"><MapPin />To do</span>
 		<p>
-			Select a city to see the locations and names of the corresponding towns and cities in the U.S.
-			Zoom in on the globe to see city names that do not initially appear. The globe will then shift
-			to the U.S., with lines drawn from the selected city to the corresponding municipalities in
-			the U.S.
+			Select a city to see the locations and names of the corresponding places in the U.S. Zoom in
+			to see city names that do not initially appear. The globe will then shift to the U.S., with
+			lines drawn from the selected city to the matching municipalities in the U.S.
 		</p>
 
 		<span class="subheader"><Table />Data source</span>
 		<p>
-			The data comes from the U.S. <a href="Direct Link to All Names Data Products"
+			The U.S. place names data comes from the U.S. <a href="Direct Link to All Names Data Products"
 				>Board on Geographic Names</a
-			>. Read more from my
+			>
+			while the international data originates from
+			<a href="https://www.naturalearthdata.com/">Natural Earth</a>
+			via the <a href="https://cran.r-project.org/web/packages/maps/"><em>maps</em> package</a> in
+			R. Read more from my
 			<a href="https://chejel.github.io/data-blog/posts/international-cities-us/"
 				>write-up of the data wrangling process</a
 			>. The post includes a link to the previous version of this map, made using vanilla
@@ -75,6 +78,7 @@
 	}
 
 	footer {
+		font-size: 0.85rem;
 		display: flex;
 		align-items: center;
 		gap: 3px;
