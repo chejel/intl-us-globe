c0fd4bce88b4d99216d7c001eb48e5912ddd138f
chejel <chejenj@gmail.com>
2024-03-15 23:47:44 -0400

    Use GH Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        appDir: 'app', // Required as the default is _app
         adapter: adapter(),
 		paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
         }
-    },
-    preprocess: vitePreprocess()
+    }
 };
 
 export default config;
